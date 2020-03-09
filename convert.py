4a39ba0f298f14f59e59371c905dc4e4ec0050bc
Daniel Eberhard <Daniel.Eberhard@rohde-schwarz.com>
2020-03-09 15:51:35 +0100

    added gui

--- convert.py
+++ convert.py
@@ -32,6 +32,11 @@ class Feuerwehr():
         self.neibors.append(wehr)
         self.neibors = list(dict.fromkeys(self.neibors))
     def getTLFs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "tlf":
+                minimum = float(row[1])
         tlfs = self.tlf
         wehren = []
         wehren.append(self)
@@ -45,9 +50,14 @@ class Feuerwehr():
                     wehren.append(neibor)
         value = tlfs/len(wehren)
         if len(wehren)<3:
-            value += 0.2
+            value += minimum/2
         return value
     def getELW1s(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "elw1":
+                minimum = float(row[1])
         elw1s = self.elw1
         wehren = []
         wehren.append(self)
@@ -60,10 +70,15 @@ class Feuerwehr():
                     elw1s += neibor.elw1
                     wehren.append(neibor)
         value = elw1s/len(wehren)
-        if len(wehren)<2:
-            value += 0.5
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getDLKs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "dlk":
+                minimum = float(row[1])
         dlks = self.dlk
         wehren = []
         wehren.append(self)
@@ -76,10 +91,15 @@ class Feuerwehr():
                     dlks += neibor.dlk
                     wehren.append(neibor)
         value = dlks/len(wehren)
-        if len(wehren)<2:
-            value += 0.25
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getRWs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "rw":
+                minimum = float(row[1])
         rws = self.rw
         wehren = []
         wehren.append(self)
@@ -92,10 +112,15 @@ class Feuerwehr():
                     rws += neibor.rw
                     wehren.append(neibor)
         value = rws/len(wehren)
-        if len(wehren)<3:
-            value += 0.2
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getHLFs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "hlf":
+                minimum = float(row[1])
         hlfs = self.hlf
         wehren = []
         wehren.append(self)
@@ -108,10 +133,15 @@ class Feuerwehr():
                     hlfs += neibor.hlf
                     wehren.append(neibor)
         value = hlfs/len(wehren)
-        if len(wehren)<3:
-            value += 0.2
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getGWOELs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "gwOel":
+                minimum = float(row[1])
         gwOels = self.gwOel
         wehren = []
         wehren.append(self)
@@ -124,10 +154,15 @@ class Feuerwehr():
                     gwOels += neibor.gwOel
                     wehren.append(neibor)
         value = gwOels/len(wehren)
-        if len(wehren)<4:
-            value += (1.0/7.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getGWAs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "gwA":
+                minimum = float(row[1])
         gwAs = self.gwA
         wehren = []
         wehren.append(self)
@@ -140,10 +175,15 @@ class Feuerwehr():
                     gwAs += neibor.gwA
                     wehren.append(neibor)
         value = gwAs/len(wehren)
-        if len(wehren)<4:
-            value += (1.0/8.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getGWSs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "gwS":
+                minimum = float(row[1])
         gwSs = self.gwS
         wehren = []
         wehren.append(self)
@@ -156,10 +196,15 @@ class Feuerwehr():
                     gwSs += neibor.gwS
                     wehren.append(neibor)
         value = gwSs/len(wehren)
-        if len(wehren)<4:
-            value += (1.0/8.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getHOEHs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "hoeh":
+                minimum = float(row[1])
         hoehs = self.hoeh
         wehren = []
         wehren.append(self)
@@ -172,10 +217,15 @@ class Feuerwehr():
                     hoehs += neibor.hoeh
                     wehren.append(neibor)
         value = hoehs/len(wehren)
-        if len(wehren)<6:
-            value += (1.0/11.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getMESSs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "mess":
+                minimum = float(row[1])
         messs = self.mess
         wehren = []
         wehren.append(self)
@@ -188,10 +238,15 @@ class Feuerwehr():
                     messs += neibor.mess
                     wehren.append(neibor)
         value = messs/len(wehren)
-        if len(wehren)<6:
-            value += (1.0/12.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getGWGs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "gwG":
+                minimum = float(row[1])
         gwGs = self.gwG
         wehren = []
         wehren.append(self)
@@ -204,10 +259,15 @@ class Feuerwehr():
                     gwGs += neibor.gwG
                     wehren.append(neibor)
         value = gwGs/len(wehren)
-        if len(wehren)<7:
-            value += (1.0/13.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getELW2s(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "elw2":
+                minimum = float(row[1])
         elw2s = self.elw2
         wehren = []
         wehren.append(self)
@@ -220,10 +280,15 @@ class Feuerwehr():
                     elw2s += neibor.elw2
                     wehren.append(neibor)
         value = elw2s/len(wehren)
-        if len(wehren)<7:
-            value += (1.0/14.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getDEKONPs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "dekonP":
+                minimum = float(row[1])
         dekonPs = self.dekonP
         wehren = []
         wehren.append(self)
@@ -236,10 +301,15 @@ class Feuerwehr():
                     dekonPs += neibor.dekonP
                     wehren.append(neibor)
         value = dekonPs/len(wehren)
-        if len(wehren)<8:
-            value += (1.0/15.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
     def getFWKs(self):
+        data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+        minimum = 0
+        for row in data:
+            if row[0] == "fwk":
+                minimum = float(row[1])
         fwks = self.fwk
         wehren = []
         wehren.append(self)
@@ -252,42 +322,43 @@ class Feuerwehr():
                     fwks += neibor.fwk
                     wehren.append(neibor)
         value = fwks/len(wehren)
-        if len(wehren)<8:
-            value += (1.0/15.0)
+        if len(wehren)<int((1.0/minimum)/2.0):
+            value += minimum
         return value
 
-wehren = []
-data = csv.reader(open('wehren.csv', "r", encoding='ansi'))
-next(data)
-for row in data:
-    wehr = Feuerwehr(row[0].strip(), row[1].strip(), row[2].strip(), row[3].strip(), row[4].strip(), row[5].strip(), row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(), row[10].strip(), row[11].strip(), row[12].strip(), row[13].strip(), row[14].strip(), row[15].strip(), row[16].strip())
-    wehren.append(wehr)
+def convert():
+    wehren = []
+    data = csv.reader(open('wehren.csv', "r", encoding='ansi'))
+    next(data)
+    for row in data:
+        wehr = Feuerwehr(row[0].strip(), row[1].strip(), row[2].strip(), row[3].strip(), row[4].strip(), row[5].strip(), row[6].strip(), row[7].strip(), row[8].strip(), row[9].strip(), row[10].strip(), row[11].strip(), row[12].strip(), row[13].strip(), row[14].strip(), row[15].strip(), row[16].strip())
+        wehren.append(wehr)
 
-data = csv.reader(open('verbindungen.csv', "r", encoding='ansi'))
-for row in data:
-    getWehr(wehren,row[0].strip()).addNeibor(getWehr(wehren,row[1].strip()))
-    getWehr(wehren,row[1].strip()).addNeibor(getWehr(wehren,row[0].strip()))
+    data = csv.reader(open('verbindungen.csv', "r", encoding='ansi'))
+    for row in data:
+        getWehr(wehren,row[0].strip()).addNeibor(getWehr(wehren,row[1].strip()))
+        getWehr(wehren,row[1].strip()).addNeibor(getWehr(wehren,row[0].strip()))
 
-data = "!!python/object:networkx.classes.graph.Graph\n"
-data += "_adj:\n"
-for wehr in wehren:
-    data += "  " + wehr.name + ":\n"
-    for neibor in wehr.neibors:
-        data += "    " + neibor.name + ": {}\n"
-data += "_node: &id001\n"
-for wehr in wehren:
-    data += "  " + wehr.name + ": {pos_x: " + wehr.pos_x + ", pos_y: " + wehr.pos_y + ", tlf: " + str(wehr.getTLFs()) + ", elw1: " + str(wehr.getELW1s()) + ", dlk: " + str(wehr.getDLKs()) + ", rw: " + str(wehr.getRWs()) + ", hlf: " + str(wehr.getHLFs()) + ", gwOel: " + str(wehr.getGWOELs()) + ", gwA: " + str(wehr.getGWAs()) + ", gwS: " + str(wehr.getGWSs()) + ", hoeh: " + str(wehr.getHOEHs()) + ", mess: " + str(wehr.getMESSs()) + ", gwG: " + str(wehr.getGWGs()) + ", elw2: " + str(wehr.getELW2s()) + ", dekonP: " + str(wehr.getDEKONPs()) + ", fwk: " + str(wehr.getFWKs()) + "}\n"
-data += "adjlist_inner_dict_factory: &id002 !!python/name:builtins.dict ''\n"
-data += "adjlist_outer_dict_factory: *id002\n"
-data += "edge_attr_dict_factory: *id002\n"
-data += "graph:\n"
-data += "  name: FFs\n"
-data += "graph_attr_dict_factory: *id002\n"
-data += "node_attr_dict_factory: *id002\n"
-data += "node_dict_factory: *id002\n"
-data += "nodes: !!python/object:networkx.classes.reportviews.NodeView\n"
-data += "  _nodes: *id001\n"
+    data = "!!python/object:networkx.classes.graph.Graph\n"
+    data += "_adj:\n"
+    for wehr in wehren:
+        data += "  " + wehr.name + ":\n"
+        for neibor in wehr.neibors:
+            data += "    " + neibor.name + ": {}\n"
+    data += "_node: &id001\n"
+    for wehr in wehren:
+        data += "  " + wehr.name + ": {pos_x: " + wehr.pos_x + ", pos_y: " + wehr.pos_y + ", tlf: " + str(wehr.getTLFs()) + ", elw1: " + str(wehr.getELW1s()) + ", dlk: " + str(wehr.getDLKs()) + ", rw: " + str(wehr.getRWs()) + ", hlf: " + str(wehr.getHLFs()) + ", gwOel: " + str(wehr.getGWOELs()) + ", gwA: " + str(wehr.getGWAs()) + ", gwS: " + str(wehr.getGWSs()) + ", hoeh: " + str(wehr.getHOEHs()) + ", mess: " + str(wehr.getMESSs()) + ", gwG: " + str(wehr.getGWGs()) + ", elw2: " + str(wehr.getELW2s()) + ", dekonP: " + str(wehr.getDEKONPs()) + ", fwk: " + str(wehr.getFWKs()) + "}\n"
+    data += "adjlist_inner_dict_factory: &id002 !!python/name:builtins.dict ''\n"
+    data += "adjlist_outer_dict_factory: *id002\n"
+    data += "edge_attr_dict_factory: *id002\n"
+    data += "graph:\n"
+    data += "  name: FFs\n"
+    data += "graph_attr_dict_factory: *id002\n"
+    data += "node_attr_dict_factory: *id002\n"
+    data += "node_dict_factory: *id002\n"
+    data += "nodes: !!python/object:networkx.classes.reportviews.NodeView\n"
+    data += "  _nodes: *id001\n"
 
-f = open("test.yml", "w")
-f.write(data)
-f.close()
+    f = open("test.yml", "w")
+    f.write(data)
+    f.close()
--- lss.py
+++ lss.py
@@ -1,30 +1,106 @@
 import matplotlib.pyplot as plt
-import csv
 import networkx as nx
-
+import tkinter as tk
 import convert
+import csv
 
-G = nx.read_yaml("test.yml")
-pos = {}
-color = []
-for key in G._node:
-       pos[key]=(G._node[key]["pos_y"],G._node[key]["pos_x"])
-#       if G._node[key]["tlf"]<2.2: color.append('red')
-#       if G._node[key]["elw1"]<0.5: color.append('red')
-#       if G._node[key]["dlk"]<0.25: color.append('red')
-       if G._node[key]["rw"]<0.2: color.append('red')
-#       if G._node[key]["hlf"]<0.2: color.append('red')
-#       if G._node[key]["gwOel"]<(1.0/7.0): color.append('red')
-#       if G._node[key]["gwA"]<(1.0/8.0): color.append('red')
-#       if G._node[key]["gwS"]<(1.0/8.0): color.append('red')
-#       if G._node[key]["hoeh"]<(1.0/11.0): color.append('red')
-#       if G._node[key]["mess"]<(1.0/12.0): color.append('red')
-#       if G._node[key]["gwG"]<(1.0/13.0): color.append('red')
-#       if G._node[key]["elw2"]<(1.0/14.0): color.append('red')
-#       if G._node[key]["dekonP"]<(1.0/15.0): color.append('red')
-#       if G._node[key]["fwk"]<(1.0/15.0): color.append('red')
-       else: color.append('green')
+def show(type):
+       convert.convert()
 
-nx.draw(G,pos,with_labels=True,node_color=color)
-plt.axis('off')
-plt.show()
+       data = csv.reader(open('settings.csv', "r", encoding='ansi'))
+       minimum=0
+       for row in data:
+              if row[0] == type:
+                     minimum = float(row[1])
+
+       G = nx.read_yaml("test.yml")
+       pos = {}
+       color = []
+       for key in G._node:
+              pos[key]=(G._node[key]["pos_y"],G._node[key]["pos_x"])
+              if G._node[key]["tlf"]<minimum and type=="tlf": color.append('red')
+              elif G._node[key]["elw1"]<minimum and type=="elw1": color.append('red')
+              elif G._node[key]["dlk"]<minimum and type=="dlk": color.append('red')
+              elif G._node[key]["rw"]<minimum and type=="rw": color.append('red')
+              elif G._node[key]["hlf"]<minimum and type=="hlf": color.append('red')
+              elif G._node[key]["gwOel"]<minimum and type=="gwOel": color.append('red')
+              elif G._node[key]["gwA"]<minimum and type=="gwA": color.append('red')
+              elif G._node[key]["gwS"]<minimum and type=="gwS": color.append('red')
+              elif G._node[key]["hoeh"]<minimum and type=="hoeh": color.append('red')
+              elif G._node[key]["mess"]<minimum and type=="mess": color.append('red')
+              elif G._node[key]["gwG"]<minimum and type=="gwG": color.append('red')
+              elif G._node[key]["elw2"]<minimum and type=="elw2": color.append('red')
+              elif G._node[key]["dekonP"]<minimum and type=="dekonP": color.append('red')
+              elif G._node[key]["fwk"]<minimum and type=="fwk": color.append('red')
+              else: color.append('green')
+
+       nx.draw(G,pos,with_labels=True,node_color=color)
+       plt.axis('off')
+       plt.show()
+
+def lf():
+       show("tlf")
+def elw1():
+       show("elw1")
+def dlk():
+       show("dlk")
+def rw():
+       show("rw")
+def hlf():
+       show("hlf")
+def gwOel():
+       show("gwOel")
+def gwA():
+       show("gwA")
+def gwS():
+       show("gwS")
+def hoeh():
+       show("hoeh")
+def mess():
+       show("mess")
+def gwG():
+       show("gwG")
+def elw2():
+       show("elw2")
+def dekonP():
+       show("dekonP")
+def fwk():
+       show("fwk")
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+button_quit = tk.Button(frame, text="QUIT", fg="red", command=quit)
+button_quit.pack(side=tk.LEFT)
+button_lf = tk.Button(frame, text="LF", command=lf)
+button_lf.pack(side=tk.LEFT)
+button_elw1 = tk.Button(frame, text="ELW1", command=elw1)
+button_elw1.pack(side=tk.LEFT)
+button_dlk = tk.Button(frame, text="DLK", command=dlk)
+button_dlk.pack(side=tk.LEFT)
+button_rw = tk.Button(frame, text="RW", command=rw)
+button_rw.pack(side=tk.LEFT)
+button_hlf = tk.Button(frame, text="HLF", command=hlf)
+button_hlf.pack(side=tk.LEFT)
+button_gwOel = tk.Button(frame, text="GW-Öl", command=gwOel)
+button_gwOel.pack(side=tk.LEFT)
+button_gwA = tk.Button(frame, text="GW-A", command=gwA)
+button_gwA.pack(side=tk.LEFT)
+button_gwS = tk.Button(frame, text="GW-S", command=gwS)
+button_gwS.pack(side=tk.LEFT)
+button_hoeh = tk.Button(frame, text="GW-Höh", command=hoeh)
+button_hoeh.pack(side=tk.LEFT)
+button_mess = tk.Button(frame, text="GW-Mess", command=mess)
+button_mess.pack(side=tk.LEFT)
+button_gwG = tk.Button(frame, text="GW-G", command=gwG)
+button_gwG.pack(side=tk.LEFT)
+button_elw2 = tk.Button(frame, text="ELW2", command=elw2)
+button_elw2.pack(side=tk.LEFT)
+button_dekonP = tk.Button(frame, text="GW-Dekon-P", command=dekonP)
+button_dekonP.pack(side=tk.LEFT)
+button_fwk = tk.Button(frame, text="FWK", command=fwk)
+button_fwk.pack(side=tk.LEFT)
+
+root.mainloop()
+
+
